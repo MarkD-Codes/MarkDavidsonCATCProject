a273206473005f0deaf84b7f27a3bfe3004a136c
Mark Davidson <mark.davidsom@gmail.com>
2024-05-07 17:09:15 +0000

    redirected all outputs to text file

--- CATCScript.sh
+++ CATCScript.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-date
-hostname
-arch
-uname -a
-uptime
-whoami
-who
-w
-top
-history
+echo "The date: ">>MarkDavidson.txt
+date>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Hostname: " >>MarkDavidson.txt
+hostname>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Architecture: " >>MarkDavidson.txt
+arch>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Username: " >>MarkDavidson.txt
+uname>>MarkDavidson.txt
+echo >>MarkDavidson.txt
+echo "System Uptime: " >>MarkDavidson.txt
+uptime>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Username: ">>MarkDavidson.txt
+whoami>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Current Users: ">>MarkDavidson.txt
+who>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "User Processes: ">>MarkDavidson.txt
+w>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Current System Overview: " >>MarkDavidson.txt
+top>>MarkDavidson.txt
+echo>>MarkDavidson.txt
+echo "Command History: ">>MarkDavidson.txt
+history>>MarkDavidson.txt
