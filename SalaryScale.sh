dcbca8174b0ce649a2022ddf86c340ee2bc10b6b
Mark Davidson <mark.davidsom@gmail.com>
2024-05-12 14:02:49 +0000

    Wrote for loop to print table of salary scale. Loop iterates for the number of years specified by the user and prints the year and the salary earned that year

--- SalaryScale.sh
+++ SalaryScale.sh
@@ -3,6 +3,9 @@
 # Prompt user to input their salary
 read -p "Please enter your current salary: " current_salary
 
+# Prompt user to enter the current year
+read -p "Please enter the current year: " current_year
+
 # Prompt user to input the number of years
 read -p "Please enter the number of years for which you want to see the salary increase: " num_years
 
@@ -21,11 +24,18 @@ then annual_increment=$(( $salary_increment * 2 ))
 	
 elif [[ $manager_boolean == N ]]
 then annual_increment=$(( $salary_increment * 1 ))
-	echo "Annual Increment: "  $annual_increment	
+	echo "Annual Increment: "  $annual_increment
+	echo ""	
 
 else echo "Invalid input given in response to 'Are you a manager?'. Please start again."
 
 fi
 
 # Print table header
-echo "Year    				 Salary"
+printf "%-10s %-20s %-10s %-20s %-10s\n" "|" "Year" "|" "Salary" "|"
+printf "%-70s\n" "----------------------------------------------------------------"
+for ((i=0; i<=$num_years; i++)); do
+    printf "%-10s %-20s %-10s %-20s %-10s\n" "|" "$current_year" "|" "$current_salary" "|"
+    current_year=$(($current_year + 1))
+    current_salary=$(($current_salary + $annual_increment))
+done
