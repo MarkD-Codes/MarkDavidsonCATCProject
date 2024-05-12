2c53162b0f6f65170449b99570600b83533dfe70
Mark Davidson <mark.davidsom@gmail.com>
2024-05-12 12:46:32 +0000

    scripted if/else if expressions to apply salary_increment twice as annual_increment in event that employee is a manager

--- SalaryScale.sh
+++ SalaryScale.sh
@@ -8,23 +8,24 @@ read -p "Please enter the number of years for which you want to see the salary i
 
 read -p "Are you a manager?(Y/N): " manager_boolean
 
-echo $manager_boolean
-
 #Prompt user to input the salary increment
 read -p "Please enter the amount by which your salary increase with each increment: " salary_increment
 
-
-# Print table header
-echo "Year    				 Salary"
 
 #Manager Y/N branching using if/else statement
 
 
 if [[ $manager_boolean == Y ]]
-	then echo "Congratulations!"
+then annual_increment=$(( $salary_increment * 2 )) 
+	echo "Annual Increment: "  $annual_increment
+	
+elif [[ $manager_boolean == N ]]
+then annual_increment=$(( $salary_increment * 1 ))
+	echo "Annual Increment: "  $annual_increment	
 
 else echo "Invalid input given in response to 'Are you a manager?'. Please start again."
 
 fi
 
-
+# Print table header
+echo "Year    				 Salary"
